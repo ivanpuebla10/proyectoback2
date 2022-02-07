d9a7bf8b0fbffc4d64eb1f435276e84adf827278
ivanpuebla10 <ivanpuebla94@gmail.com>
2022-02-07 11:42:42 +0100

    subiendo keys ahora si

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    MONGO_URI: 'mongodb+srv://ivanpuebla10:38447202iP@cluster0.ijbjh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    jwt_secret: "mimamamemima",
+    AUTH: {
+        user: 'nodemailerprueba1@gmail.com',
+        pass: 'IvanNodemailer99'
+    }
+}
+
