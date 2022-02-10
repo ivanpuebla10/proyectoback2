b2d0e4d5af2aebbf460b531a805588383986f0c2
ivanpuebla <94801699+ivanpuebla10@users.noreply.github.com>
2022-02-10 16:17:56 +0100

    Delete keys.js

--- config/keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    MONGO_URI: 'mongodb+srv://ivanpuebla10:38447202iP@cluster0.ijbjh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    jwt_secret: "mimamamemima",
-    AUTH: {
-        user: 'nodemailerprueba1@gmail.com',
-        pass: 'IvanNodemailer99'
-    }
-}
-
