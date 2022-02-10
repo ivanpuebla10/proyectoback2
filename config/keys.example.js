fe26050898cedc8499b0910b7271af5378ff1482
ivanpuebla10 <ivanpuebla94@gmail.com>
2022-02-10 16:26:33 +0100

    keys example fixed

--- config/keys.example.js
+++ config/keys.example.js
@@ -1,3 +1,8 @@
 module.exports = {
-    MONGO_URI: 'mongodb+srv://<name>:<password>@cluster0-tuqrv.mongodb.net/test?retryWrites=true&w=majority'
+    MONGO_URI: 'mongodb+srv://<name>:<password>@cluster0-tuqrv.mongodb.net/test?retryWrites=true&w=majority',
+    jwt_secret: "your secret",
+    AUTH: {
+        user: 'your email',
+        pass: 'your password'
+    }
 }
