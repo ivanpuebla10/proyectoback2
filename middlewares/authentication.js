43cf54707ba3affa195f8bf929f96ad051a37b3a
ivanpuebla10 <ivanpuebla94@gmail.com>
2022-03-08 09:55:08 +0100

    trying

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -32,5 +32,15 @@ const isAuthor = async(req, res, next) => {
     }
 }
 
+const isAdmin = async(req, res, next) => {
+    const admins = ['admin'];
+    if (!admins.includes(req.user.role)) {
+        return res.status(403).send({
+            message: 'You do not have permission'
+        });
+    }
+    next();
+}
 
-module.exports = { authentication, isAuthor }
+
+module.exports = { authentication, isAuthor, isAdmin }
--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router()
 const PostController = require('../controllers/PostController');
-const { authentication, isAuthor  } = require("../middlewares/authentication");
+const { authentication, isAuthor, isAdmin  } = require("../middlewares/authentication");
 const { uploadUserProductsImages } = require('../middlewares/multer');
 
 router.post('/',authentication, uploadUserProductsImages.single('imagePost'), PostController.create);
 router.put('/:_id', authentication, isAuthor, uploadUserProductsImages.single('imagePost'), PostController.update);
 router.delete('/:_id', authentication, isAuthor, PostController.delete);
+router.delete('/admin/:_id', authentication, isAdmin, PostController.delete);
 router.get('/',PostController.getAll);
 router.get('/title/:title',PostController.getPostsByName);
 router.get('/id/:_id',PostController.getById);
