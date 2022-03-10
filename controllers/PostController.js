b727529c6cab8604885931ae85eeb6de8bd2b45c
ivanpuebla10 <ivanpuebla94@gmail.com>
2022-03-10 23:40:56 +0100

    another populate added

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -85,7 +85,7 @@ const PostController = {
 
   async getById(req, res) {
     try {
-      const post = await Post.findById(req.params._id).populate('userId');
+      const post = await Post.findById(req.params._id).populate('userId', "comments.userId");
       res.send(post);
     } catch (error) {
       console.error(error);
